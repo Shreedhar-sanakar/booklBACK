f184a5c3e2899f102efe4fb72689a00ea23529a7
SHREEDHAR  SANAKAR <99680746+Shreedhar-sanakar@users.noreply.github.com>
2023-12-04 12:30:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://shridhar:Android@cluster0.yabjtzy.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://shridhar:Android@cluster0.yabjtzy.mongodb.net/books-collection?retryWrites=true&w=majority';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
